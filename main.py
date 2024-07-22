8b3d34f424a33d8388015deb29252df7486fdd73
Zel <Zelgehai@gmail.com>
2024-07-22 12:37:01 -0700

    Removed Code from Main into Class

--- main.py
+++ main.py
@@ -1,84 +1,28 @@
 import cv2 as cv
 import numpy as np
-#pyautogui -> automates keyboard + mouse movements [also allows locateonScreen]
 import os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+from time import time
+from windowcapture import WindowCapture
 
-def findClickPositions(needle_image_path, default_image_path, threshold = 0.6, debug_mode=None):
+#WindowCapture.list_window_names()
+#exit()
 
-    default_image = cv.imread(default_image_path, cv.IMREAD_REDUCED_COLOR_2) #IMREAD_UNCHANGED
-    needle_image = cv.imread(needle_image_path, cv.IMREAD_REDUCED_COLOR_2)
-    needle_w = needle_image.shape[1]
-    needle_h = needle_image.shape[0]
+#create object
+wincap = WindowCapture('Diablo II: Resurrected')
 
-    #calls matchtemplate
-    method = cv.TM_CCOEFF_NORMED
-    result = cv.matchTemplate(default_image, needle_image, method) #TM_SQDIFF_NORMED
-    #returns confidence score
-    #print(result)
+loop_time = time()
+while(True):
+    screenshot = wincap.get_screenshot()
 
-   
-    locations = np.where(result >= threshold)
-    #np.where looks like this:
-    #(array([334, 335]), array([91, 91]))
-    #print(locations)
+    cv.imshow('Computer Vision', screenshot)
 
-    #zip those into position tuples
-    locations = list(zip(*locations[::-1]))
-    #print(locations)
+    #print('FPS {}'.format(1 / (time() - loop_time)))
+    loop_time = time()
 
-    #bulding list
-    rectangles = []
-    for loc in locations:
-        rect = [int(loc[0]), int(loc[1]), needle_w, needle_h] #build rectangle for each location
-        rectangles.append(rect)
-        rectangles.append(rect)
+    #'pressing "Q" will exit
+    #Waits 1ms every loop
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
 
-    rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5) #Grouping all rectangles that are close to eachother
-    print(rectangles)
-
-    points = []
-    #printing box
-    if len(rectangles):
-        print('Found Needle.')
-
-        needle_w = needle_image.shape[1]
-        needle_h = needle_image.shape[0]
-        line_color = (0,255,0)
-        line_type = cv.LINE_4
-        marker_color = (255,0,255)
-        marker_type = cv.MARKER_CROSS
-
-        #need to loop over all locations and draw their rectangle
-        for (x,y,w,h) in rectangles:
-            center_x = x + int(w/2)
-            center_y = y + int(h/2)
-            #save the points
-            points.append((center_x,center_y))
-
-            if debug_mode == 'rectangles':
-                #determine box positions:
-                top_left = (x,y)
-                bottom_right = (x+w, y+h)
-                #Draw box
-                cv.rectangle(default_image, top_left, bottom_right, line_color, line_type)
-            elif debug_mode == 'points':
-                cv.drawMarker(default_image, (center_x,center_y), marker_color, marker_type)
-            elif debug_mode == 'both':
-                top_left = (x,y)
-                bottom_right = (x+w, y+h)
-                #Draw box
-                cv.rectangle(default_image, top_left, bottom_right, line_color, line_type)
-                #Draw Marker
-                cv.drawMarker(default_image, (center_x,center_y), marker_color, marker_type)
-        
-        if debug_mode:
-            cv.imshow('Matches', default_image)
-            cv.waitKey()
-            #cv.imwrite('result.jpg', default_image)
-    
-    return points
-
-points = findClickPositions('zombie_1.jpg','img_1.jpg',
-                            threshold=0.70, debug_mode= 'both') #'points' 'rectangles' 'both'
-print(points)
+print('Done.')
