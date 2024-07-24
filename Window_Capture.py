4abe24785d607a1db4116a49b1697946ae65227f
Zel <Zelgehai@gmail.com>
2024-07-24 10:19:44 -0700

    fixed errors

--- Window_Capture.py
+++ Window_Capture.py
@@ -1,6 +1,5 @@
 import numpy as np
 import win32gui, win32ui, win32con
-import mss
 from ctypes import windll
 
 class WindowCapture:
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 import os
 from time import time
-from windowcapture import WindowCapture
+from Window_Capture import WindowCapture
 
 #WindowCapture.list_window_names()
 #exit()
